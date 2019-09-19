b6c23560af8e45c4cce402c1d2d3d728fadd2f17
Sergio <31887668+sramos02@users.noreply.github.com>
2019-09-19 19:59:16 +0200

    Create 116.cpp

---
+++ 116.cpp
@@ -0,0 +1,18 @@
+//HOLA MUNDO
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int main() {
+	unsigned short n;
+	cin >> n;
+
+	if (n == 1) cout << "Hola mundo.\n";
+	else if (n == 2) cout << "Hola mundo.\nHola mundo.\n";
+	else if (n == 3) cout << "Hola mundo.\nHola mundo.\nHola mundo.\n";
+	else if (n == 4) cout << "Hola mundo.\nHola mundo.\nHola mundo.\nHola mundo.\n";
+	else if (n == 5) cout << "Hola mundo.\nHola mundo.\nHola mundo.\nHola mundo.\nHola mundo.\n";
+
+	return 0;
+}
