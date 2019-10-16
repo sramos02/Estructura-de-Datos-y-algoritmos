eee34a856f039ec0b3c354ca74933ca28f3f12e2
Sergio <31887668+sramos02@users.noreply.github.com>
2019-10-16 18:54:16 +0200

    Update 156.cpp

--- 156.cpp
+++ 156.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int resuelveCaso(int v[]){
-    int i = 1, ret = 0;
-
-    while(v[i] != -1){
-        int abs = (v[i - 1] - v[i]);
-        if(abs < 0) abs *= -1;
-
-        ret += abs;
+bool resuelveCaso(){
+    int v[10000];
+    int abs;
+    int i = 0, ret = 0;
+    bool fin = false;
+    
+    cin >> v[0]; i++;
+    if(v[0] == -1) return true; //Ha llegado al final del programa
+    
+    while(!fin){
+        cin >> v[i];
+        if(v[i] != -1) {
+            abs = (v[i - 1] - v[i]);        
+            if(abs < 0) abs *= -1;
+            ret += abs;
+        }
+        else{ 
+            cout << ret << endl;
+            fin = true;
+        }
         i++;
     }
 
-    return ret;
+    return false;
 }
 
 int main(){
-    int v[10000];
-    int i = 1;
-    bool fin = false;
-
-    //Lee hasta que encuentra el -1 y lo guarda todo en v
-
+        bool fin = false;
         while(!fin){
-            cin >> v[i];
-            //Aqui ha encontrado el -1 y ha metido todo lo anterior en v
-            if(v[i] == -1)
-                if(i == 0)fin = true; 
-                else {
-                    cout << resuelveCaso(v) << endl; 
-                    i = 0;
-                }
+          fin = resuelveCaso();
         }
     return 0;
 }
