39129de3f5f1df13ed6068dd94b0f51b13c90c0d
Sergio <31887668+sramos02@users.noreply.github.com>
2019-10-17 10:58:47 +0200

    Update 156.cpp

--- 156.cpp
+++ 156.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 using namespace std;
 
-bool resuelveCaso(){
-    int v[10000];
-    int abs;
-    int i = 0, ret = 0;
-    bool fin = false;
-    
-    cin >> v[0]; i++;
-    if(v[0] == -1) return true; //Ha llegado al final del programa
-    
-    while(!fin){
-        cin >> v[i];
-        if(v[i] != -1) {
-            abs = (v[i - 1] - v[i]);        
-            if(abs < 0) abs *= -1;
-            ret += abs;
-        }
-        else{ 
-            cout << ret << endl;
-            fin = true;
-        }
-        i++;
-    }
+bool resuelveCaso() {
+	int v[2]; //Vamos comparando todo el rato los ultimos dos que nos entran
+	int abs, aux;
+	int i = 0, ret = 0;
+	bool fin = false;
 
-    return false;
+	cin >> v[0]; i++; //Mete el primero y prepara el segundo
+	if (v[0] < 0) return true; //Ha llegado al final del programa
+
+	while (!fin) {
+		cin >> v[i];
+		if (v[i] >= 0) {
+			if((aux = (i - 1)) < 0) aux = -aux; //Se supone que -1%2 = 1 pero no lo procesa
+			abs = (v[aux] - v[i]);  
+			if (abs < 0) abs *= -1;
+			ret += abs;
+		}
+		else {
+			cout << ret << endl;
+			fin = true;
+		}
+		i = ((i + 1) % 2);
+	}
+
+	return false;
 }
 
-int main(){
-        bool fin = false;
-        while(!fin){
-          fin = resuelveCaso();
-        }
-    return 0;
+int main() {
+	bool fin = false;
+	while (!fin) {
+		fin = resuelveCaso();
+	}
+	return 0;
 }
