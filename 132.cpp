8a327b9990014694207013810b009628e164fef9
Sergio <31887668+sramos02@users.noreply.github.com>
2019-10-17 11:38:42 +0200

    Update 132.cpp

--- 132.cpp
+++ 132.cpp
@@ -2,44 +2,48 @@
 #include <string>
 using namespace std;
 
-bool resuelveCaso(const string palabra){
+bool resuelveCaso(const string palabra) {
 
-    int aux, i, j;
-    cin >> i >> j;
-    
-    //I siempre será la posición mínima
-    if(i > j) {
-        aux = i;
-        i = j;
-        j = aux;
-    }
-    
-    while(i <= j){
-        if(palabra[i] != palabra[j]) return false;
-        i++;
-    }
-    
-    return true;
+	int aux, i, j;
+	cin >> i >> j;
+
+	//I siempre será la posición mínima
+	if (i > j) {
+		aux = i;
+		i = j;
+		j = aux;
+	}
+
+	while (i <= j) {
+		if (palabra[i] != palabra[j]) return false;
+		i++;
+	}
+
+	return true;
 }
 
-int main(){
-    
-    string palabra;
-    int numCasos; 
-    int i = 0;
-    
-    cin >> palabra;
-    cin >> numCasos;
-        
-    while(i < numCasos){
-        cout << (resuelveCaso(palabra) ? "SI"  : "NO") << endl;
-        i++;
-        
-        if(i == numCasos){
-            cout << endl;
-            cin >> palabra;
-            cin >> numCasos;
-        }
-    }
-    return 0;
+int main() {
+
+	string palabra;
+	int numCasos;
+	int i = 0;
+
+	getline(cin, palabra);
+	cin >> numCasos;
+
+	while (i < numCasos) {
+		cout << (resuelveCaso(palabra) ? "SI" : "NO") << endl;
+		i++;
+
+		if (i == numCasos) {
+			cout << endl;
+
+			cin.get();
+			getline(cin, palabra);
+
+			cin >> numCasos;
+			i = 0;
+		}
+	}
+	return 0;
 }
