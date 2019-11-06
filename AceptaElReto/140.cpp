897e7347dfffa94690aa50d7c15cda6e330c35d9
Sergio <31887668+sramos02@users.noreply.github.com>
2019-11-06 17:51:49 +0100

    Create 140.cpp

---
+++ AceptaElReto/140.cpp
@@ -0,0 +1,35 @@
+//https://www.aceptaelreto.com/problem/statement.php?id=140
+#include <iostream>
+using namespace std;
+
+
+int resuelveCaso(int n, int aux) {
+	int acum = 0;
+
+	if (n == 0);
+	else if (n < 10) {
+		acum = resuelveCaso(0, aux + n % 10) + n%10;
+		cout << n % 10;
+	}
+	else {
+		acum = resuelveCaso(n / 10, aux + n % 10) + n % 10;
+		cout << " + " << n % 10;
+	}
+
+	return acum;
+}
+
+int main() {
+	
+	int n;
+	cin >> n;
+
+	while (n != -1) {
+		int aux = resuelveCaso(n, 0);
+		cout << " = "<< aux << endl;
+		cin >> n;
+	}
+
+	system("Pause");
+	return 0;
+}
