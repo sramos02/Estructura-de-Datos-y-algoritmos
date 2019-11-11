99913b47c4d9b5aeca01822653a34421cd423979
Sergio <31887668+sramos02@users.noreply.github.com>
2019-11-11 17:28:10 +0100

    Update 171.cpp

--- AceptaElReto/171.cpp
+++ AceptaElReto/171.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 using namespace std;
 
-
-bool resuelveCaso() {
-	int n;
-	int ret = 0, mayor = 0;
-
-	int v[100000];
-
-	cin >> n;
-	if (n == 0) return false;
-
-
-	for (int i = 0; i < n; i++)	cin >> v[i];
-
-	for (int i = n - 1; i >= 0; i--) {
-		if (v[i] > mayor) {
-			mayor = v[i];
-			ret++;
-		}
+//P = {0 <= n <= v.length}
+int solve(int v[], int n){
+    int ret = 0, hg = 0;
+    for (int i = n - 1; i >= 0; i--) {
+	if (v[i] > hg) {
+		hg = v[i];
+		ret++;
 	}
+    }
+    return ret;
+}
+//Q = {ret = max(u: n-1 >= i >= 0 && v[u] > hg: V[i]}
 
-	cout << ret << endl;
-	return true;
+bool solveCase() {
+    int n;
+    int v[100000];
+
+    cin >> n;
+    if (n == 0) return false;
+
+    for (int i = 0; i < n; i++)	cin >> v[i];
+    cout << solve(v, n) << endl;
+    return true;
 }
 
 int main() {
-	while (resuelveCaso());
+	while (solveCase());
 	return 0;
 }
