f9be925bd549ea42f468bb0aa8a4814b652d9283
Sergio <31887668+sramos02@users.noreply.github.com>
2019-11-11 19:25:38 +0100

    Update 156.cpp

--- AceptaElReto/156.cpp
+++ AceptaElReto/156.cpp
@@ -2,37 +2,36 @@
 #include <iostream>
 using namespace std;
 
-bool resuelveCaso() {
-	int v[2]; //Vamos comparando todo el rato los ultimos dos que nos entran
-	int abs, aux;
-	int i = 0, ret = 0;
-	bool fin = false;
+bool solve(int v[]){
+	int v[2];       //Vamos comparando todo el rato los ultimos dos que nos entran
+    int abs, aux;
+    int  ret = 0, i = 1;
+    bool end = false;
 
-	cin >> v[0]; i++; //Mete el primero y prepara el segundo
+	cin >> v[0];    //Mete el primero y prepara el segundo
 	if (v[0] < 0) return true; //Ha llegado al final del programa
 
-	while (!fin) {
-		cin >> v[i];
-		if (v[i] >= 0) {
-			if((aux = (i - 1)) < 0) aux = -aux; //Se supone que -1%2 = 1 pero no lo procesa
-			abs = (v[aux] - v[i]);  
-			if (abs < 0) abs *= -1;
-			ret += abs;
-		}
-		else {
-			cout << ret << endl;
-			fin = true;
-		}
-		i = ((i + 1) % 2);
-	}
 
-	return false;
+    while (!end) {
+            cin >> v[i];
+            if (v[i] >= 0) {
+                if((aux = (i - 1)) < 0) aux = -aux;  //WARNING Se supone que -1%2 = 1 pero no lo procesa
+                abs = (v[aux] - v[i]);  
+                if (abs < 0) abs *= -1;
+                ret += abs;
+            }
+            else {
+                cout << ret << endl;
+                end = true;
+            }
+            i = ((i + 1) % 2);
+        }
+
+    return end;
 }
 
 int main() {
-	bool fin = false;
-	while (!fin) {
-		fin = resuelveCaso();
-	}
+	bool end = false;
+	while (!solve());
 	return 0;
 }
