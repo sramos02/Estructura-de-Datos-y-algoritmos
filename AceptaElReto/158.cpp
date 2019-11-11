7cadbe3773b9c5e84746cd003f39a6a7e15163cc
Sergio <31887668+sramos02@users.noreply.github.com>
2019-11-11 19:33:11 +0100

    Update 158.cpp

--- AceptaElReto/158.cpp
+++ AceptaElReto/158.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void resuelveCaso(long long int n){
-    int i = 1, v[2];           //Auxiliares
-    int arriba = 0, abajo = 0; //Salidas
-
+//P = {0 < n < 10^9}
+void solve(long long int n, int & up, int & down){
+    int i = 1, v[2];
     cin >> v[0]; 
     
     while(i < n){
         cin >> v[i%2];
-        if(v[(i - 1)%2] < v[i%2]) arriba++;
-        else if (v[(i - 1)%2] > v[i%2]) abajo++;
+        if(v[(i - 1)%2] < v[i%2]) up++;
+        else if (v[(i - 1)%2] > v[i%2]) down++;
         i++;
     }
-    
-    cout << arriba << " " << abajo << endl;
 }
+//Q = {up = #u : 1 <= u < n : v[u-1] < v[u] && down = #u : 1 <= u < n : v[u-1] > v[u]}
 
 int main(){
-    int numCasos; 
-    int i = 0;
+    int cases;
 
-    cin >> numCasos;
-        
-    while(i < numCasos){
+    cin >> cases;
+    for(int i = 0; i < cases; i++){
         long long int n;
+        int up = 0, down = 0; 
+        
         cin >> n;
-        resuelveCaso(n);
-        i++;
+        solve(n, up, down);
+        cout << up <<  " " << down << endl;
     }
     
     return 0;
