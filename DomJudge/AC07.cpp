ada682373d1dee8c1991e5974ac4036ab59d0873
Sergio <31887668+sramos02@users.noreply.github.com>
2019-12-11 19:31:08 +0100

    Create AC07.cpp

---
+++ DomJudge/AC07.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//P = {0 < datos.size() && 0 < minimo}
+vector<int> laCasaDeHeidi(vector<int> datos, int minimo, int & secMasLarga, int & numLlanos) {
+	vector<int> resultado;
+	int j = datos.size() - 1, i;
+
+	//Busca mayor y solo comprueba despues de él
+	int altMax = datos[datos.size() - 1];
+	int longitud = 1;
+
+	while (j > 0) {
+		if (datos[j - 1] == datos[j]) {
+			longitud++;
+			if (longitud >= minimo && longitud > secMasLarga) secMasLarga = longitud;
+		}
+		else {
+			if (datos[j - 1] < datos[j]);
+			else altMax = datos[j];
+
+
+			if (longitud > minimo) {
+				resultado.push_back(longitud - j + 1); 
+				numLlanos++;
+			}
+
+			j--;
+			longitud = 1;
+		}
+	}
+	return resultado;
+}
+//Q = resultado(u) = {PT u, w :(0 <= u <= w < datos.size()) && (PT k: u <= k <= w: datos[k] = datos.at[u] && esAccesible(datos, k)) : ((w - u) >= minimo)}
+// esAccesible(datos, k) = {PT v: k <= v < w : datos[v] <= datos[k]}
+
+bool resuelveCaso() {
+	int n, minimo;
+	int secMasLarga = 0, numLlanos = 0;
+	vector<int> datos;
+	
+	cin >> n >> minimo;
+	for (int i = 0; i < n; i++) {
+		int aux;
+		cin >> aux;
+		datos.push_back(aux);
+	}
+
+	vector<int> ret = laCasaDeHeidi(datos, minimo, secMasLarga, numLlanos);
+
+	//Mostrar solución
+	cout << secMasLarga << " " << numLlanos << " ";
+	for (int i = 0; i < numLlanos; i++) cout << ret[i] << " ";
+	cout << endl;
+
+	//Retorno
+	if (secMasLarga == 0 && numLlanos == 0) return false;
+	return true;
+}
+
+int main() {
+	while (resuelveCaso());
+	return 0;
+}
