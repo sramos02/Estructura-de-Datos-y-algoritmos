e53beb6eb09d222d03498eb6a644a65dc776d4a4
Sergio <31887668+sramos02@users.noreply.github.com>
2019-12-16 18:58:15 +0100

    Update AC11.cpp

--- DomJudge/AC11.cpp
+++ DomJudge/AC11.cpp
@@ -2,29 +2,31 @@
 #include <math.h>
 using namespace std;
 
-void comp(int n, int & complementario, int & complementarioInv){
-   if(n < 10) {
-       complementario = 9-n;
-       complementarioInv = complementario;
-   }
-   else{
-        comp(n/10, complementario, complementarioInv);
-        complementario    = complementario * pow(10, log10(n)) + ((9 - (n%10)));
-        complementarioInv = ((9 -(n%10))) * pow(10, log10(n)) + complementarioInv;  
-    }
+void comp(int n, int & complementario, int & complementarioInv) {
+	if (n < 10) {
+		complementario = 9 - n;
+		complementarioInv = complementario;
+	}
+	else {
+		comp(n / 10, complementario, complementarioInv);
+		int cmp = 9 - (n % 10);
+		complementario = complementario * 10 + cmp;
+		complementarioInv = cmp * (pow(10, (int)log10(n))) + complementarioInv;
+	}
 }
 
 
-int main(){
-   int casos;
-   cin >> casos;
-   for(int i = 0; i < casos; i++){
-        long int n;
-        cin >> n;
-        
-        int res, resInv;
-        comp(n, res, resInv);
-        cout << res << " " << resInv << endl;
-   }
-  return 0;
+int main() {
+	int casos;
+	cin >> casos;
+	for (int i = 0; i < casos; i++) {
+		long int n;
+		cin >> n;
+
+		int res, resInv;
+		comp(n, res, resInv);
+		cout << res << " " << resInv << endl;
+	}
+	system("PAUSE");
+	return 0;
 }
