c7affa1e8d67fa0a6b4a59b6843c8220558ebd3b
Sergio <31887668+sramos02@users.noreply.github.com>
2019-12-19 22:51:45 +0100

    Update AC13.cpp

--- DomJudge/AC13.cpp
+++ DomJudge/AC13.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 
-char quienFalta(vector<char> datos, int ini, int fin) {
-    int iniAux = datos[0];
-    if (ini + 1 == fin) return datos[ini] - 1; //Vector de un solo elemento
+int curvaConcava(vector<int> datos, int ini, int fin) {
+	if (ini + 1 == fin) return datos[ini]; //Vector de un solo elemento
 
 	int m = (ini + fin - 1) / 2;
-	if (m > 0 && (iniAux + m) < iniAux-datos[m]) return quienFalta(datos, ini, m);
-	else if (m < datos.size() && (iniAux + m) < iniAux-datos[m]) return quienFalta(datos, m + 1, fin);
-	return datos[m] + 1;
+	if (m > 0 && datos[m - 1] < datos[m]) return curvaConcava(datos, ini, m);
+	else if (m < datos.size() && datos[m + 1] < datos[m]) return curvaConcava(datos, m + 1, fin);
+	return datos[m];
+		
 }
 
 
@@ -20,18 +20,15 @@ bool resuelveCaso() {
 	cin >> n;
 	if (!cin) return false;
 
-	char primero, ultimo, aux;
-	vector<char> datos;
+	vector<int> datos;
 
-    cin >> primero;
-	cin >> ultimo;
-		
-	for(int i = 0; i < (ultimo - primero); i++){    
+	for(int i = 0; i < n; i++){
+		int aux;
 		cin >> aux;
 		datos.push_back(aux);
 	}
 
-	cout << quienFalta(datos, 0, datos.size()) << endl;
+	cout << curvaConcava(datos, 0, datos.size()) << endl;
 	return true;
 }
 
