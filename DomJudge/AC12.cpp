3e0ec56e9f34e25f0e2b02f24d8e5047af40b311
Sergio <31887668+sramos02@users.noreply.github.com>
2019-12-22 09:47:10 +0100

    Update AC12.cpp

--- DomJudge/AC12.cpp
+++ DomJudge/AC12.cpp
@@ -3,23 +3,16 @@
 using namespace std;
 
 
-int buscaMinimo(const vector<int> datos, int ini, int fin) {
-		
-	if (datos.size() == 2) {
-		if(datos[ini] < datos[fin - 1]) return datos[ini];
-		else return datos[fin - 1];
-	}
+int curvaConcava(vector<int> datos, int ini, int fin) {
+	if (ini + 1 == fin) return datos[ini]; //Vector de un solo elemento
 
-	if (ini + 1 == fin) return datos[ini];
-
-	int m = (ini + (fin - 1)) / 2;
-	if (m > 0 && m < datos.size()) {
-		if (datos[m - 1] > datos[m] && datos[m + 1] < datos[m]) return buscaMinimo(datos, m + 1, fin);
-		else if (datos[m - 1] < datos[m] && datos[m + 1] < datos[m]) return buscaMinimo(datos,ini, m);
-		else return datos[m];
-	}
+	int m = (ini + fin - 1) / 2;
+	if (m > 0 && datos[m - 1] < datos[m]) return curvaConcava(datos, ini, m);
+	else if (m < datos.size() && datos[m + 1] < datos[m]) return curvaConcava(datos, m + 1, fin);
 	return datos[m];
+		
 }
+
 
 
 bool resuelveCaso() {
@@ -29,13 +22,13 @@ bool resuelveCaso() {
 
 	vector<int> datos;
 
-	for (int i = 0; i < n; i++) {
+	for(int i = 0; i < n; i++){
 		int aux;
 		cin >> aux;
 		datos.push_back(aux);
 	}
 
-	cout << buscaMinimo(datos, 0, datos.size()) << endl;
+	cout << curvaConcava(datos, 0, datos.size()) << endl;
 	return true;
 }
 
