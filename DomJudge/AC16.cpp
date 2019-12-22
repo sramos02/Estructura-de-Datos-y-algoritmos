e97046345a73eca7763eb52ac3eb93b4e00705e8
Sergio <31887668+sramos02@users.noreply.github.com>
2019-12-22 18:33:40 +0100

    Update AC16.cpp

--- DomJudge/AC16.cpp
+++ DomJudge/AC16.cpp
@@ -2,27 +2,52 @@
 #include <vector>
 using namespace std;
 
-bool elDegradado(const vector<vector<int>> matriz, int ini, int fin){
-    //SIN TERMINAR
+struct degradado {
+    int suma;
+    bool correcto;
+};
+
+
+degradado elDegradado(const vector<int> matriz, const int ini, const int fin){
+    //Caso base
+    if(ini == fin) return {matriz[ini], true};
+
+    //Caso recursivo
+    degradado izq, drc;
+    int m = (ini + fin) / 2;
+
+    izq = elDegradado(matriz, ini, m);
+    drc = elDegradado(matriz, m + 1, fin);
+    return{izq.suma + drc.suma, izq.correcto && drc.correcto && izq.suma < drc.suma};
 }
 
 bool resuelveCaso(){
     int n, m;
-    while(cin >> n >> m){
+    bool ret = true;
 
-        vector<vector<int>> matriz;
-        for(int i = 0; i < n; i++){
-            vector<int> fila;
-            for(int j = 0; j < m; j++){
-                int aux;
-                cin >> aux;
-                fila.push_back(aux);
-            }
-            matriz.push_back(fila);
+    cin >> n >> m;
+    if(!cin) return  false;
+
+
+    vector<vector<int>> matriz;
+    for(int i = 0; i < n; i++){
+        vector<int> fila;
+        for(int j = 0; j < m; j++){
+            int aux;
+            cin >> aux;
+            fila.push_back(aux);
         }
-
-        elDegradado(matriz, 0, matriz.size() - 1)?cout << "SI\n":cout << "NO\n";
+        matriz.push_back(fila);
     }
+
+    //Si hay un solo elemento no hay degradado
+    if(m > 1){
+        for(int i = 0; i < n && ret; i++){
+            ret = elDegradado(matriz[i], 0, matriz.size() - 1).correcto;
+        }
+    }
+    (ret)?cout << "SI\n":cout << "NO\n";
+    return true;
 }
 
 int main(){
