cfe686e71e895b9a7a4583ca22aa625c9ff20cce
Sergio <31887668+sramos02@users.noreply.github.com>
2019-12-25 13:08:13 +0100

    Update and rename 2018_ENE to 2018_ENE.cpp

--- Examenes/2018_ENE.cpp
+++ Examenes/2018_ENE.cpp
@@ -1,24 +1,27 @@
 //Ejercicio 2
-  #include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
+//Coste O(logN) en relación con el numero de elementos del vector
+//El vector divide a la mitad todo el rato hasta encontrar un numero igual o mayor y uno menor
 int elCercano(const vector<int> datos, const int x, int ini, int fin, int mejorSol){
 
     //Calculo para dividir
     int m = (ini + fin) / 2;
 
     //Actualización
-    if(x < datos[m]) for(int i = m; m > 0 && datos[i] == datos[m]; i--)m--;
-    else for(int i = m; m < datos.size()-1 && datos[i] == datos[m]; i++)m++;
-
     if (abs(x - datos[m]) < abs(x - datos[mejorSol])) mejorSol = m;
+
+    if(x <= datos[m]) for(int i = 0; m > 0 && datos[m - i - 1] == datos[m]; i--) m--;
+    else  for(int i = 0; m < datos.size()-1 && datos[m + i + 1] == datos[m]; i++)m++;
+
 
     //Movimiento
 
     if(abs(mejorSol - m) == 1) return datos[mejorSol];
-    else if(m > 0 && x < datos[m]) elCercano(datos, x, ini, m, mejorSol);
-    else if (m < datos.size() && x > datos[m]) return elCercano(datos, x, m + 1, fin, mejorSol);
+    else if(m > 0 && x <= datos[m]) elCercano(datos, x, ini, m, mejorSol);
+    else if (m < datos.size() && x >= datos[m]) return elCercano(datos, x, m + 1, fin, mejorSol);
 
 }
 
@@ -40,3 +43,6 @@ int main(){
 
     return 0;
 }
+
+
+//Ejercicio 3
