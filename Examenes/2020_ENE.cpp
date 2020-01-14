988940fd6057013a1823ac1cbf5bf3f17567f523
Sergio <31887668+sramos02@users.noreply.github.com>
2020-01-14 10:45:02 +0100

    Update 2020_ENE.cpp

--- Examenes/2020_ENE.cpp
+++ Examenes/2020_ENE.cpp
@@ -39,3 +39,44 @@ int main(){
         cin >> n;   
     }
 }
+
+//Ejercicio 2
+bool entretenido(unsigned long long n){
+
+    bool ret = true;
+    int i = log10(n) - 1;
+
+    while(i >= 0 && ret){
+        if((long long int)(n%10) == (long long int)(n%100/10)) ret = false;
+        n = n/10;
+        i--;
+    }
+    return ret;
+}
+
+//La complejidad de este algoritmo es O(N) ya que se crean N iteraciones, cada una de ellas
+//de coste constante O(1), el parametro k inicializado a 0 aumenta hasta llegar a N (caso base)
+
+int cuantosEntretenidos(const unsigned long long n, unsigned long long k){
+    int act = 0, sig = 0;
+    
+    if(k == n) {
+        if(entretenido(n)) return 1;
+        else return 0;
+    }
+    else{
+        if(entretenido(k))act=1;
+        if(cuantosEntretenidos(n, k+1))sig=1;
+    }
+    
+    return act + sig;
+}
+
+int main(){
+    unsigned long long n;
+    cin >> n;
+    while(n =! -1){
+        cout << cuantosEntretenidos(n, 0) << endl;
+        cin >> n;   
+    }
+}
